9a71f64745427f7d6a98c1430438c32fdd90892b
Sebastian Tromer <tromersebastian@gmail.com>
2018-03-07 21:13:32 +0100

    Initial commit with cmake to enable build

---
+++ src/simple_reader.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Named Pipes - Simple reader" << std::endl;
+
+    return 0;
+}
+
---
+++ src/simple_writer.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Named Pipes - Simple writer" << std::endl;
+
+    return 0;
+}
+
